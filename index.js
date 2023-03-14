ecc2bbd08280aba33f6f73b5b8e943fa86e1644f
Masha Gorbunova <masha.gorbunova@gmail.com>
2023-03-15 00:04:32 +0300

    add -> emptyField, emptySearch and fields with name, url and author for repos

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-let searchBtn   = document.querySelector('#searchBtn');
-let search      = document.querySelector('#search');
+let searchBtn = document.querySelector('#searchBtn');
+let search = document.querySelector('#search');
 
-
-searchBtn.addEventListener('click', () => {
-    let searchValue = search.value;
-    if(searchValue !== ""){
-        fetch(`https://api.github.com/search/repositories?q=${searchValue}`)
-            .then(res => res.json())
-            .then(data => setReposField(JSON.stringify(data)))
-    } else {
-        console.log('else')
+search.addEventListener('keypress', (e) => {
+    let key = e.keyCode || e.which;
+    if (key === 13) { // Клавиша Enter
+        console.log('enter');
+        getRepos();
     }
-    search.value = "";
 });
 
-function setReposField(res){
+searchBtn.addEventListener('click', getRepos);
+
+function getRepos() {
+    let searchValue = search.value;
+    if (searchValue !== "") {
+        fetch(`https://api.github.com/search/repositories?q=${searchValue}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.total_count === 0) {
+                    emptyField()
+                } else {
+                    setReposField(JSON.stringify(data))
+                }
+            })
+    } else {
+        console.log('else')
+        emptySearch()
+    }
+    search.value = "";
+}
+
+
+function setReposField(res) {
     let reposField = document.getElementById('repoField');
     let out = '';
     let reposObj = JSON.parse(res);
     let repos = reposObj.items.splice(0, 10)
     console.log(repos)
-    repos.map( item => {
-        out += `<p>Наименование: ${item.name}</p>`
+    repos.map(item => {
+        out += `<p><a href="${item.html_url}" target="_blank">Наименование: ${item.name}</a></p>`
+        out += `<p><a href="${item.owner.html_url}" target="_blank">Автор: ${item.owner.login}</a></p>`
+        out += `<p>Описание: ${item.description}</p>`
+        out += `<p>Язык: ${item.language}</p>`
 
     });
 
     reposField.innerHTML = out;
+}
+
+function emptyField() {
+    let emptyField = document.getElementById('repoField');
+    emptyField.innerHTML = `<p>ничего не найдено</p>`;
+}
+
+function emptySearch() {
+    let emptySearch = document.getElementById('repoField');
+    emptySearch.innerHTML = `<p>а вы ничего и не ввели!</p>`;
 }
